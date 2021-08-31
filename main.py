64445604b280fd08fce0a86f8ac5c95ceb3e57ba
Anushka Purohit <88351265+anushkahit@users.noreply.github.com>
2021-08-31 21:37:22 +0000

    added code

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
 import py_midicsv as pm
-import pandas as pd
+#import pandas as pd
 import csv
 import random
+import requests
+
+
 
 import os
 from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
 
-def midicsv:
+@app.route('/')
+def render_main():
+
+    return render_template('home.html')
+
+def download_link():
+    url = 'https://www.facebook.com/favicon.ico'#replace with var from template using request.args[]
+    r = requests.get(url, allow_redirects=True)
+
+    open('facebook.ico', 'wb').write(r.content)#replace with var from template
+download_link()
+
+
+@app.route('/midi_csv')
+def render_midi_csv():
+    try:
+
+        
+
+        midi_csv()
+
+        return render_template('midi_csv.html')
+
+    except ValueError:
+
+        return "Sorry: something went wrong."
+
+    #return render_template('midi_csv.html')
+
+@app.route('/download_csv')
+def render_download():
+    try:
+        download_link()
+        
+
+def midi_csv():
 # Load the MIDI file and parse it into CSV format
-    csv_string = pm.midi_to_csv("TheWeeknd-BlindingLights.mid")
+    #call download_link()
+    csv_string = pm.midi_to_csv("ShapeofYou.mid")
 
     notes_list = []
 
@@ -66,16 +105,10 @@ def midicsv:
             except KeyError:
                 key = (key[1], random.choice(list(chain[key])))
             doesnt_work = False
-        
-
-            
-            
-    
-    
-    j+=1
+        j+=1
     #print(markov_new_notes)
 
-    csv_string1 = pm.midi_to_csv("TheWeeknd-BlindingLights.mid")
+    csv_string1 = pm.midi_to_csv("ShapeofYou.mid")
     with open("example_converted1.csv", "w") as f:
         f.writelines(csv_string1)
 
@@ -128,7 +161,10 @@ def midicsv:
     midi_object = pm.csv_to_midi("example_converted1.csv")
 
     # Save the parsed MIDI file to disk
-    with open("blinding.mid", "wb") as output_file:
+    with open("ShapeofYou_ringtone1.mid", "wb") as output_file:
         midi_writer = pm.FileWriter(output_file)
         midi_writer.write(midi_object)
 
+if __name__ == "__main__":
+
+   app.run(host='0.0.0.0', debug=True)
